aac8b9403ff07c6e469f46655a96d9cc09c2e339
Christopher <chris.kuut@pm.me>
2025-11-24 17:31:42 +0200

    grid is resizable and does thing when hover over now + sneak peek hehe

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
-let count = 0;
-const square_count_limit = 100;
+let row_column_count = 0;
+const row_column_count_max = 100;
+let grid_exists = 0;
 
-let create_count = 0;
-let remove_count = 0;
-
-const btn = document.querySelector(".btn");
+const btn_grid = document.querySelector(".grid");
 const container = document.querySelector(".container");
 const squares = document.querySelectorAll(".square");
 
-btn.addEventListener("click", onButtonClick);
+btn_grid.addEventListener("click", buttonClickedGrid);
+container.addEventListener("mouseenter", fill, true);
+container.addEventListener("mouseleave", empty, true);
 
-// TODO need to figure out how to make hovering work again
-squares.forEach((sq) => sq.addEventListener("mouseenter", (e) => {
-  sq.style.backgroundColor = "black";
-}));
-
-squares.forEach((sq) => sq.addEventListener("mouseleave", (e) => {
-  setTimeout(() => {
-    sq.style.backgroundColor = "";
-  }, 200);
-}));
-
-function onButtonClick() {
-  count = prompt("How many squares (1 - 100)?");
-  if (count > square_count_limit) count = square_count_limit;
-  console.log(count);
+function buttonClickedGrid() {
+  row_column_count = prompt("How many rows and columns (1 - 100)?");
+  if (row_column_count > row_column_count_max) row_column_count = row_column_count_max;
   createGrid();
-  console.log(create_count);
-  console.log(remove_count);
 }
 
 function createGrid() {
-  if (create_count > remove_count) removeGrid();
+  if (grid_exists === 1) removeGrid();
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < row_column_count; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
-    for (let j = 0; j < count; j++) {
+    for (let j = 0; j < row_column_count; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
       row.appendChild(square);
     }
     container.appendChild(row);
   }
-  create_count++;
+  grid_exists = 1;
 }
 
 function removeGrid() {
   const rows = document.querySelectorAll(".row");
   rows.forEach((row) => row.remove());
-  remove_count++;
+}
+
+function fill(e) {
+  if (e.target.classList.contains("square")) {
+    e.target.style.backgroundColor = "black";
+  }
+}
+
+function empty(e) {
+  if (e.target.classList.contains("square")) {
+    setTimeout(() => {
+      e.target.style.backgroundColor = "";
+    }, 200);
+  }
 }
